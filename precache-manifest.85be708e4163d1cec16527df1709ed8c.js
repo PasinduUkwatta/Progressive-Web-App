4315dffbad879cc7ee44f519b379de06495212a7
Pasindu Ukwatta <pasindu.17@itfac.mrt.ac.lk>
2021-07-09 20:26:46 +0530

    Updates

---
+++ precache-manifest.85be708e4163d1cec16527df1709ed8c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff6931545c26d59e0e415384bea91d05",
+    "url": "/Progressive-Web-App/index.html"
+  },
+  {
+    "revision": "0f67f05ecb2023e73b9a",
+    "url": "/Progressive-Web-App/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "44da30ca475322f5e310",
+    "url": "/Progressive-Web-App/static/css/main.62cb83db.chunk.css"
+  },
+  {
+    "revision": "0f67f05ecb2023e73b9a",
+    "url": "/Progressive-Web-App/static/js/2.8fd48abf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Progressive-Web-App/static/js/2.8fd48abf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44da30ca475322f5e310",
+    "url": "/Progressive-Web-App/static/js/main.dd877832.chunk.js"
+  },
+  {
+    "revision": "97e21a97ceca19830155",
+    "url": "/Progressive-Web-App/static/js/runtime-main.dce711e7.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Progressive-Web-App/static/media/SEGA.12a3db8b.woff"
+  }
+]);
